17c78866fc6ae5702a0f54977c2a714aa8940e2c
ivanceasa <ceaivan777@gmail.com>
2021-08-13 10:13:12 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronouns = ["the", "our"];
+let adjs = ["great", "big", "unique"];
+let nouns = ["jogger", "racoon"];
+let extensions = [".com", ".net"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let ul = document.createElement("ul");
+  let domain = [];
+  for (let i = 0; i < pronouns.length; i++) {
+    let pronoun = pronouns[i];
+    for (let j = 0; j < adjs.length; j++) {
+      let adj = adjs[j];
+      for (let k = 0; k < nouns.length; k++) {
+        let noun = nouns[k];
+        for (let l = 0; l < extensions.length; l++) {
+          let extension = extensions[l];
+          domain = "www." + pronoun + adj + noun + extension;
+          let li = document.createElement("li");
+          li.innerHTML = domain;
+          ul.append(li);
+        }
+      }
+    }
+  }
+  document.body.append(ul);
 };
